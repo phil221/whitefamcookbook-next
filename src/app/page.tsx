a489a5efc7afc0cebe45a6afc59a00df3ac9c93b
Phil Meckley <pomeckley@gmail.com>
2024-06-09 15:19:13 -0400

    add dummy recipes list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import RecipesList from "@/components/RecipesList";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Recipes
+    <main className="flex min-h-screen flex-col p-20 gap-3">
+      <h1 className="text-3xl">Recipes</h1>
+      <section>
+        <RecipesList />
+      </section>
     </main>
   );
 }

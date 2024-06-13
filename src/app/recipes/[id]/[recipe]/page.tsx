5a523c65eae03ee7c7bdbb29b06e44a8ebbb08b3
phil221 <pomeckley@gmail.com>
2024-06-13 16:25:36 -0400

    add metadata object for recipe pages

--- src/app/recipes/[id]/[recipe]/page.tsx
+++ src/app/recipes/[id]/[recipe]/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import React from "react";
 import { getRecipe } from "../../../../../lib/recipes";
+import { Metadata, ResolvingMetadata } from "next";
+import { SITE_TITLE } from "@/constants";
 
 type Props = {
   params: {
@@ -9,8 +11,22 @@ type Props = {
   };
 };
 
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  
+  const recipe = await getRecipe(params.id);
+  const parentTitle = (await parent).title?.absolute || SITE_TITLE;
+  console.log(parentTitle)
+ 
+  return {
+    title: `${parentTitle} | ${recipe?.name ?? 'Recipe'}`,
+  }
+}
+
 export default async function Recipe({ params }: Props) {
-  const recipe  = await getRecipe(parseFloat(params.id));
+  const recipe  = await getRecipe(params.id);
 
   return (
     <div>

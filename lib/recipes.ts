fef48bea68c249cd8bd3c523a3d1bdd1c13a6ee6
phil221 <pomeckley@gmail.com>
2024-06-13 16:02:50 -0400

    add get single recipe fn

--- lib/recipes.ts
+++ lib/recipes.ts
@@ -2,10 +2,22 @@ import prisma from "./prisma";
 
 export async function getRecipes() {
     try {
-        const recipes = await prisma.recipe.findMany()
+        const recipes = await prisma.recipes.findMany()
         return recipes;
     } catch (error) {
         console.error("getRecipes error:", error);
+        return [];
+    }
+}
+
+export async function getRecipe(recipeid: number) {
+    try {
+        const recipe = await prisma.recipes.findUnique({
+            where: { recipeid }
+        })
+        return recipe;
+    } catch (error) {
+        console.error("getRecipe error:", error);
         return [];
     }
 }

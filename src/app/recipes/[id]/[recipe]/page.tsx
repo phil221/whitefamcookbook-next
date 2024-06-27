f4f5a859a7edb522e3d7de0ad57214accd4e032c
phil221 <pomeckley@gmail.com>
2024-06-27 16:43:54 -0400

    clean up date time format utility and use in recipe page

--- src/app/recipes/[id]/[recipe]/page.tsx
+++ src/app/recipes/[id]/[recipe]/page.tsx
@@ -4,6 +4,7 @@ import { getRecipe } from "@lib/recipes";
 import { Metadata, ResolvingMetadata } from "next";
 import { SITE_TITLE } from "@/constants";
 import { composePathFromString } from "@/utils/path";
+import getPrettyDate from "@/utils/date";
 
 type Props = {
   params: {
@@ -28,10 +29,14 @@ export default async function Recipe({ params }: Props) {
   const recipe = await getRecipe(params.id);
   const authorPath = composePathFromString(recipe?.authorName || "");
   const categoryPath = composePathFromString(recipe?.category.name || "");
+  const createdTime = recipe?.createdAt?.getTime() || new Date().getTime();
   return (
+    <div className="flex flex-col gap-4">
     <div>
       <h1 className="text-xl">{recipe?.name}</h1>
-      <div className="flex gap-10">
+      <p className="text-xs">Added on {getPrettyDate(createdTime)}</p>
+    </div>
+      <div>
       <p>
         By <Link href={`/authors/${recipe?.author.id}/${authorPath}`}>{recipe?.authorName}</Link>
       </p>
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,22 +1,15 @@
 /**
- * Returns a formatted date string in the format 'Month Day, Year' based on the provided timestamp string.
- * If the timestamp string is falsy, returns 'N/A'.
+ * Returns a formatted date string in the format 'Month Day, Year' based on the provided timestamp.
  *
- * @param {string} timestampString - The timestamp string to convert to a date string.
+ * @param {number} timestamp - The timestamp to convert to a date string.
  * @return {string} The formatted date string in the format 'Month Day, Year'.
  */
-const getPrettyDate = (timestampString: string): string => {
-    if (!timestampString) {
-      return 'N/A';
-    }
-  
-    const nextRenewalDate = new Date(timestampString.concat('T00:00:00'));
-  
+const getPrettyDate = (timestamp: number) => {
     return new Intl.DateTimeFormat('en-US', {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
-    }).format(nextRenewalDate);
+    }).format(timestamp);
   };
   
   export default getPrettyDate;

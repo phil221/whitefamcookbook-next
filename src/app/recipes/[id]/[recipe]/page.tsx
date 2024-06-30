62116acd9a5582ee89b580f32806da007bffc975
Phil Meckley <pomeckley@gmail.com>
2024-06-30 15:40:45 -0400

    wip parsing ingredients for recipe template

--- src/app/recipes/[id]/[recipe]/page.tsx
+++ src/app/recipes/[id]/[recipe]/page.tsx
@@ -30,19 +30,46 @@ export default async function Recipe({ params }: Props) {
   const authorPath = composePathFromString(recipe?.authorName || "");
   const categoryPath = composePathFromString(recipe?.category.name || "");
   const createdTime = recipe?.createdAt?.getTime() || new Date().getTime();
+  const ingredients = recipe?.ingredients!;
+  const fmtItems = ingredients.split("\n").map((item, i) => {
+    return item.trim();
+  });
+
+  const readyItems = fmtItems.reduce((prev, _, i, fmtItems) => {
+    const pair = [fmtItems[i * 2], fmtItems[i * 2 + 1]];
+    prev.push(pair);
+    return prev;
+  }, [] as string[][]);
+  console.log({ readyItems });
   return (
-    <div className="flex flex-col mx-auto content-center gap-4 max-w-screen-lg">
-      <div>
+    <div className="flex flex-col mx-auto content-center gap-2 max-w-screen-lg">
+      <div className="flex flex-col content-center text-center gap-1 flex-wrap">
         <h1 className="text-xl">{recipe?.name}</h1>
-        <p className="text-xs">Added on {getPrettyDate(createdTime)}</p>
+        <Link
+          className="font-medium"
+          href={`/authors/${recipe?.author.id}/${authorPath}`}
+        >
+          {recipe?.authorName}
+        </Link>
+        {/* <p className="text-xs">Added on {getPrettyDate(createdTime)}</p> */}
+      </div>
+      <div className="section flex justify-between flex-wrap">
+        <p>Makes {recipe?.servingsNumber} servings</p>
+        <p>Prep Time: {recipe?.prepTime} minutes</p>
+      </div>
+      <hr className="border-black" />
+      <div className="section ingredients">
+        <ul>
+          {recipe?.ingredients.split("\n").map((ingredient, i) => (
+            <>
+              <li key={ingredient} className="w-fit">
+                {ingredient}
+              </li>{" "}
+            </>
+          ))}
+        </ul>
       </div>
       <div>
-        <p>
-          By{" "}
-          <Link href={`/authors/${recipe?.author.id}/${authorPath}`}>
-            {recipe?.authorName}
-          </Link>
-        </p>
         <Link href={`/categories/${recipe?.category.id}/${categoryPath}`}>
           {recipe?.category.name}
         </Link>

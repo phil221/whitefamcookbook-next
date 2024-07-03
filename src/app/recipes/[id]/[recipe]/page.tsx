447f1c56ce25adc886d1019278f459dbe8a7e920
phil221 <pomeckley@gmail.com>
2024-07-03 14:47:35 -0400

    basic recipe view

--- src/app/recipes/[id]/[recipe]/page.tsx
+++ src/app/recipes/[id]/[recipe]/page.tsx
@@ -4,7 +4,6 @@ import { getRecipe } from "@lib/recipes";
 import { Metadata, ResolvingMetadata } from "next";
 import { SITE_TITLE } from "@/constants";
 import { composePathFromString } from "@/utils/path";
-import getPrettyDate from "@/utils/date";
 
 type Props = {
   params: {
@@ -27,20 +26,30 @@ export async function generateMetadata(
 
 export default async function Recipe({ params }: Props) {
   const recipe = await getRecipe(params.id);
+  const {
+    
+  } = recipe || {};
   const authorPath = composePathFromString(recipe?.authorName || "");
   const categoryPath = composePathFromString(recipe?.category.name || "");
-  const createdTime = recipe?.createdAt?.getTime() || new Date().getTime();
   const ingredients = recipe?.ingredients!;
-  const fmtItems = ingredients.split("\n").map((item, i) => {
-    return item.trim();
-  });
+  const instructions = recipe?.instructions!;
+  const formattedIngredients = ingredients.split("\n").map(item => item.trim());
+  const formattedInstructions = instructions.split("\n").map(item => item.trim());
 
-  const readyItems = fmtItems.reduce((prev, _, i, fmtItems) => {
-    const pair = [fmtItems[i * 2], fmtItems[i * 2 + 1]];
-    prev.push(pair);
-    return prev;
-  }, [] as string[][]);
-  console.log({ readyItems });
+  const getPairs = (items: string[]) => {
+    return items.reduce((prev, current, i) => {
+      if(i % 2 === 0) {
+        prev.push([current]);
+        return prev;
+      } else {
+        prev[prev.length - 1].push(current);
+        return prev;
+      }
+    }, [] as string[][]);
+  };
+
+  const readyIngredients = getPairs(formattedIngredients);
+  
   return (
     <div className="flex flex-col mx-auto content-center gap-2 max-w-screen-lg">
       <div className="flex flex-col content-center text-center gap-1 flex-wrap">
@@ -51,7 +60,6 @@ export default async function Recipe({ params }: Props) {
         >
           {recipe?.authorName}
         </Link>
-        {/* <p className="text-xs">Added on {getPrettyDate(createdTime)}</p> */}
       </div>
       <div className="section flex justify-between flex-wrap">
         <p>Makes {recipe?.servingsNumber} servings</p>
@@ -60,23 +68,44 @@ export default async function Recipe({ params }: Props) {
       <hr className="border-black" />
       <div className="section ingredients">
         <ul>
-          {recipe?.ingredients.split("\n").map((ingredient, i) => (
+          {readyIngredients.map((ingredientPair, i) => (
+            <li key={i} className="w-100">
+              <div className="flex justify-between">
+                <p>{ingredientPair[0]}</p>
+                <p>{ingredientPair[1]}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="section instructions my-3">
+        <ul>
+          {formattedInstructions.map((instruction, i) => (
             <>
-              <li key={ingredient} className="w-fit">
-                {ingredient}
-              </li>{" "}
+              <li key={i} className="w-100">
+                {instruction}
+              </li>
             </>
           ))}
         </ul>
       </div>
-      <div>
+      {
+        recipe?.comment && (
+          <div className="section comments italic mb-2">
+            <p>{recipe?.comment}</p>
+          </div>
+        )
+      }
+      <div className="section category">
         <Link href={`/categories/${recipe?.category.id}/${categoryPath}`}>
-          {recipe?.category.name}
+          <p className="font-medium">{recipe?.category.name}</p>
         </Link>
       </div>
-      <p>{JSON.stringify(recipe)}</p>
-      <p>...</p>
-      <Link href="/">Back to Home</Link>
+      <hr className="border-black" />
+      <div className="section nutrition-facts">
+        <p>{recipe?.nutritionFacts}</p>
+      </div>
+      <Link href="/">Back to all recipes</Link>
     </div>
   );
 }

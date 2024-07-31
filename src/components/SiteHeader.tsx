80355bede065d197980a2a32da6bb9c663748c3e
phil221 <pomeckley@gmail.com>
2024-07-31 14:50:48 -0400

    extracted nav links and added hover effects

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import NavLink from "./header/NavLink";
 
 type Props = {
 
@@ -8,7 +9,7 @@ type Props = {
 const SiteHeader = ({ }: Props) => {
 
     return (
-        <header className="bg-beige">
+        <header className="bg-beige-100 border-b-[0.25px] border-gray-950 border-double sticky top-0">
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <Link href="/">
@@ -24,11 +25,11 @@ const SiteHeader = ({ }: Props) => {
                     </Link>
                 </div>
                 <div className="hidden lg:flex lg:gap-x-4 align-center">
-                    <Link className="text-md font-medium" href={"/"}>Recipes</Link>
+                    <NavLink href={"/"} text="Recipes" />
                     <div className="w-px my-auto h-5 bg-gray-900" />
-                    <Link className="text-md font-medium" href={"/authors"}>Authors</Link>
+                    <NavLink href={"/authors"} text="Authors" />
                     <div className="w-px my-auto h-5 bg-gray-900" />
-                    <Link className="text-md font-medium" href={"/categories"}>Categories</Link>
+                    <NavLink href={"/categories"} text="Categories" />
                 </div>
             </nav>
         </header>
---
+++ src/components/header/NavLink.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react'
+
+type Props = {
+    href: string;
+    text: string;
+    children?: React.ReactNode;
+}
+
+const NavLink = ({ href, text, children }: Props) => {
+
+    return (
+        <Link className="text-md font-medium border-b border-transparent hover:border-gray-950 ease-in-out duration-300 hover:font-semibold" href={href}>{children ?? text}</Link>
+    );
+}
+
+export default NavLink;

7b2806d7cdf1f9e53df7636eab19e582d768ac2c
Phil Meckley <pomeckley@gmail.com>
2024-08-04 14:55:38 -0400

    spcaing and sizing edits

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -20,15 +20,15 @@ export default async function Recipes({ searchParams }: Props) {
   const categories = await getCategories();
 
   return (
-    <main className="flex flex-col gap-8">
-      <h1 className="text-3xl font-semibold mt-8">Recipes</h1>
+    <main className="flex flex-col gap-4">
+      <h1 className="text-3xl font-semibold mt-5">Recipes</h1>
       <div className="flex gap-5">
-        <section className="border-[0.25px] border-gray-950 rounded-md max-h-56 overflow-scroll w-3/12 p-5">
+        <section className="border-[0.25px] border-gray-950 rounded-md max-h-[60vh] overflow-scroll w-3/12 p-5">
           <AuthorFilters authors={authors} />
           <div className="w-100 my-4 h-px bg-gray-900" />
           <CategoryFilters categories={categories} />
         </section>
-        <section className="border-[0.25px] border-gray-950 rounded-md max-h-56 overflow-scroll w-6/12 p-5">
+        <section className="border-[0.25px] border-gray-950 rounded-md max-h-[60vh] overflow-scroll w-6/12 p-5">
           <RecipesList recipes={recipes} />
         </section>
       </div>

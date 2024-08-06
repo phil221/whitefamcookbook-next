3860e906d5d2ef35bb141eeb6f29b67e1468235a
phil221 <pomeckley@gmail.com>
2024-08-06 17:16:28 -0400

    add cn util to support conditions and style merging, added to baseLink

--- src/components/shared/BaseLink.tsx
+++ src/components/shared/BaseLink.tsx
@@ -1,16 +1,35 @@
-import Link from 'next/link';
-import React from 'react'
+"use client"
+
+import cn from '@/utils/cn';
+import Link, { LinkProps } from 'next/link';
+import React, { useEffect } from 'react'
 
 type Props = {
     href: string;
     text: string;
     children?: React.ReactNode;
-}
+    className?: string;
+} & LinkProps;
 
-const NavLink = ({ href, text, children }: Props) => {
+const NavLink = ({ href, text, children, className, ...props }: Props) => {
+    const [active, setActive] = React.useState(false)
+
+    useEffect(() => {
+        console.log({ href })
+        setActive(window ? window.location.pathname === href : false)
+    }, [])
 
     return (
-        <Link className="text-md font-medium border-b border-transparent hover:border-gray-950 ease-in-out duration-300 hover:font-semibold w-fit" href={href}>{children ?? text}</Link>
+        <Link
+            className={cn("text-md font-medium border-b border-transparent hover:border-gray-950 ease-in-out duration-300 hover:font-semibold w-fit", className, {
+                "border-gray-950": active,
+                "font-semibold": active
+            })}
+            href={href}
+            {...props}
+        >
+            {children ?? text}
+        </Link>
     );
 }
 

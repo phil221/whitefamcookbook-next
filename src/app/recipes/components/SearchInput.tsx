309cc5464a64970d519c84dc3e718bca67b80eaf
phil221 <pomeckley@gmail.com>
2024-08-20 16:16:23 -0400

    make search input realtime

--- src/app/recipes/components/SearchInput.tsx
+++ src/app/recipes/components/SearchInput.tsx
@@ -2,35 +2,29 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
-type Props = {
-
-}
-
-const SearchInput = ({ }: Props) => {
+const SearchInput = () => {
     const searchParams = useSearchParams();
+    const router = useRouter();
     const [searchValue, setSearchValue] = useState("");
 
-    const search = () => {
+    const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
         const params = new URLSearchParams(searchParams.toString());
-        if (searchValue.length > 0) params.set("q", searchValue);
-        return params.toString();
+        searchValue.length > 0 ? params.set("q", e.target.value) : params.delete("q");
+        router.push(`/recipes?${params.toString()}`);
     }
 
     return (
         <div className='flex gap-3'>
             <input
-                onChange={(e) => setSearchValue(e.target.value)}
+                onChange={search}
                 className="border-[0.25px] border-gray-950 rounded-md p-1 w-6/12"
                 value={searchValue}
                 type="text"
             />
-            <Link href={`/recipes?${search()}`}>
-                <button className='border-[0.25px] border-gray-950 rounded-md p-1'>
-                    Search
-                </button>
-            </Link>
         </div>
     );
 }

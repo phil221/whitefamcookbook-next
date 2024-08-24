e578e94eaaf75e53dfa264950352520c459cd5cb
Phil Meckley <pomeckley@gmail.com>
2024-08-24 15:38:17 -0400

    exclude search param value from filters count

--- src/app/recipes/components/ClearFilterText.tsx
+++ src/app/recipes/components/ClearFilterText.tsx
@@ -1,22 +1,32 @@
 "use client";
 
-import BaseLink from '@/components/shared/BaseLink';
-import { useSearchParams } from 'next/navigation';
-import React from 'react'
-
+import BaseLink from "@/components/shared/BaseLink";
+import { useSearchParams } from "next/navigation";
+import React from "react";
 
 const ClearFilterText = () => {
-    const searchParams = useSearchParams();
-    const filtersAmount = searchParams.size;
+  const searchParams = useSearchParams();
+  const params = new URLSearchParams(searchParams.toString());
 
-    if (filtersAmount === 0) return null;
+  const searchValue = params.get("q");
+  const filtersAmount =
+    searchValue && searchValue.length > 0
+      ? searchParams.size - 1
+      : searchParams.size;
 
-    return (
-        <div className="flex gap-5">
-            <p>{filtersAmount === 1 ? "1 Filter" : `${filtersAmount} Filters`} applied.</p>
-            <BaseLink href="/recipes" text={`Clear Filter${filtersAmount > 1 ? "s" : ""}`} />
-        </div>
-    );
-}
+  if (filtersAmount === 0) return null;
+
+  return (
+    <div className="flex gap-5">
+      <p>
+        {filtersAmount === 1 ? "1 Filter" : `${filtersAmount} Filters`} applied.
+      </p>
+      <BaseLink
+        href="/recipes"
+        text={`Clear Filter${filtersAmount > 1 ? "s" : ""}`}
+      />
+    </div>
+  );
+};
 
 export default ClearFilterText;

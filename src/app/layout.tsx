7c610bb7697ec93cf4045d11a2d7b160b9182298
phil221 <pomeckley@gmail.com>
2024-08-29 13:32:10 -0400

    change site layout to grid, auto adjust main sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,11 +20,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-beige-100 text-primary">
-      <body className={`${cormorant.className} h-screen`}>
+      <body className={`${cormorant.className} h-screen grid grid-rows-[auto_1fr_auto]`}>
         <SiteHeader />
-        <div className="max-w-7xl mx-auto h-[calc(100vh_-_82px)] p-6 lg:px-8">
+        <div className="max-w-7xl mx-auto p-6 lg:px-8 w-full">
           {children}
         </div>
+        <footer className="h-20 bg-beige-200 border-t-[0.25px] border-gray-950 border-double">
+          <div className="max-w-7xl mx-auto h-full p-6 lg:px-8">
+            <p className="text-center text-gray-800">
+              &copy; 2022 The White Family Cookbook
+            </p>
+          </div>
+        </footer>
       </body>
     </html>
   );

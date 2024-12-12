724719f8cb9100c97d6a0ccc0c293fc02dc0f514
Phil Meckley <pomeckley@gmail.com>
2024-12-12 16:31:31 -0500

    update tailwind config content to point at updated dirs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,19 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         beige: {
           100: "#fdf5e6",
-          200: "#fffaf5"
+          200: "#fffaf5",
         },
-        primary: "#0a263d"
-      }
+        primary: "#0a263d",
+      },
     },
   },
   plugins: [],
